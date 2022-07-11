1e51b4ba33755d424b1bed9e4dc7b47d80ce281c
fajrian <fajrian.publicmail@gmail.com>
2022-07-11 14:27:11 +0700

    episode - 12 - bool opereration

--- belajar-golang/bool-operation.go
+++ belajar-golang/bool-operation.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main()  {
-	
+	var nilaiAkhir = 80
+	var nilaiAbsensi = 90
+	//var totalNilai bool = nilaiAkhir >= 80
+	//var totalAbsen bool = nilaiAbsensi >= 80
+
+	//var total = totalAbsen && totalNilai
+	//fmt.Println(total)
+	fmt.Println(nilaiAkhir >= 80 && nilaiAbsensi >= 80)
 }
