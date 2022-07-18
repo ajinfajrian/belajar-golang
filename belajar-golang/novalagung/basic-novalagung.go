573ad031a1ed1e6d9374d0d2dfe5363c50b3c0c2
fajrian <fajrian.publicmail@gmail.com>
2022-07-18 10:36:25 +0700

    add new novalagung dir

---
+++ belajar-golang/novalagung/basic-novalagung.go
@@ -0,0 +1,11 @@
+package main
+import(
+	"fmt"
+)
+
+func main() {
+	one := "satu"
+	Str,Intgr,float,bole := "string", 1, 2.2, true // interference multi tipe data
+	fmt.Println(one)
+	fmt.Println(Str,Intgr,float,bole)
+}
